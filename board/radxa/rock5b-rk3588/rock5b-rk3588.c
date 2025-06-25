24d9b73bb0df1cacef475fb54ecdd40c067eacce
Wang Haoyang <haoyangw@gmail.com>
2025-06-25 13:13:06 +0000

    board: rock5b-rk3588: Fix ft_board_setup() method signature
    -Change 'bd' argument type from bd_info to bd_t
      to match the method signature declared in
      fdt_support.h of our older U-Boot source

--- board/radxa/rock5b-rk3588/rock5b-rk3588.c
+++ board/radxa/rock5b-rk3588/rock5b-rk3588.c
@@ -7,7 +7,7 @@
 #include <fdt_support.h>
 
 #ifdef CONFIG_OF_BOARD_SETUP
-int ft_board_setup(void *blob, struct bd_info *bd)
+int ft_board_setup(void *blob, bd_t *bd)
 {
 	if (IS_ENABLED(CONFIG_TYPEC_FUSB302))
 		fdt_status_okay_by_compatible(blob, "fcs,fusb302");
